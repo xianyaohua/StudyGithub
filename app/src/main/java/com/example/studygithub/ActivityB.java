854f71b1a6c3682979f8eb4a71c4af87c6e9989f
ayao <ayaojpz@protonmail.com>
2019-11-06 18:08:23 +0800

    在branchB中添加文件activityB

---
+++ app/src/main/java/com/example/studygithub/ActivityB.java
@@ -0,0 +1,19 @@
+package com.example.studygithub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ActivityB extends Activity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //remote branch b 添加的文件
+    }
+}
--- app/src/main/java/com/example/studygithub/MainActivity.java
+++ app/src/main/java/com/example/studygithub/MainActivity.java
@@ -13,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         //我是master。
         //pull到所有分支，push tags中选all时，没看到效果。
         //在master中修改，但是push到branchA中。
+        //push对话框出来时，上面的本地仓库分支和远程分支都是可以选择的。push的格式是：git push 远程主机 本地分支:远程分支
+        //
+
+        //git中的使用规则：不直接修改主分支，如果需要添加功能，就新创建一个分支。待开发测试完成再将代码合并到主分支。
     }
 }
