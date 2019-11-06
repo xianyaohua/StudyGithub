78b63bb390ba9d465ad03c272998bb0f28b03132
ayao <ayaojpz@protonmail.com>
2019-11-06 16:41:09 +0800

    在master分支中修改，但是push到branchA中。

--- app/src/main/java/com/example/studygithub/MainActivity.java
+++ app/src/main/java/com/example/studygithub/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //我是master
-        //pull到所有分支
+        //我是master。
+        //pull到所有分支，push tags中选all时，没看到效果。
+        //在master中修改，但是push到branchA中。
     }
 }
